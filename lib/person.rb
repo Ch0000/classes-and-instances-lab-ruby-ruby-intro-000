d398351034b82022172f8283cdf1dcf3403ea87d
Christian P Andrews <cpandrews91@gmail.com>
2020-06-23 03:22:08 +0000

    Automatically backed up by Learn

---
+++ lib/person.rb
@@ -0,0 +1,13 @@
+class Dog
+end
+ 
+@fido = Dog.new
+@snoopy = Dog.new
+@lassie = Dog.new 
+
+class Person
+  end
+  #make variables available across different objects
+@adele_goldberg = Person.new
+@alan_kay = Person.new
+
